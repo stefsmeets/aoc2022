817217db42d73de8443c86dd674dc4f90fecb706
Stef Smeets <s.smeets@esciencecenter.nl>
2022-11-30 11:29:39 +0100

    Day 0

---
+++ day00/solve.py
@@ -0,0 +1,48 @@
+import numpy as np
+import argparse
+import os
+from pathlib import Path
+from helpers import timeit
+
+
+DATA = Path(__file__).with_name('data.txt').read_text()
+
+
+@timeit
+def part1(s: str):
+	lines = s.splitlines()
+
+	for line in lines:
+		pass
+
+	# breakpoint()
+
+	return 0
+
+@timeit
+def part2(s: str):
+	lines = s.splitlines()
+
+	for line in lines:
+		pass
+
+	# breakpoint()
+
+	return 0
+
+
+def main() -> int:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-p', '--parts', nargs='+', type=int, choices=(1,2), default=(1,2))
+    parser.add_argument('data', nargs='?', default=DATA)
+    args = parser.parse_args()
+
+    for i in args.parts:
+    	func = (..., part1, part2)[i]
+    	func(args.data)
+
+    return 0
+
+
+if __name__ == '__main__':
+    raise SystemExit(main())
