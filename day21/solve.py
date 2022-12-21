0ede87a0c26895e3797e218b873455697c72c408
Stef Smeets <s.smeets@esciencecenter.nl>
2022-12-21 19:31:49 +0100

    Solve using imaginary number method

--- day21/solve.py
+++ day21/solve.py
@@ -1,3 +1,4 @@
+from operator import add, mul, sub, truediv
 from pathlib import Path
 
 
@@ -7,21 +8,13 @@ def parse(s: str):
     known = {}
     to_solve = {}
 
-    ops = {
-        '+': lambda x, y: x + y,
-        '-': lambda x, y: x - y,
-        '*': lambda x, y: x * y,
-        '/': lambda x, y: x / y,
-    }
+    ops = {'+': add, '-': sub, '*': mul, '/': truediv}
 
     for line in lines:
-        name = line[0:4]
-
         match line.split():
             case (name, x, op, y):
                 name = name[:-1]
-                f = ops[op]
-                to_solve[name] = (f, x, y)
+                to_solve[name] = (ops[op], x, y)
             case (name, x):
                 name = name[:-1]
                 known[name] = int(x)
@@ -56,15 +49,10 @@ def part2(s: str):
 
     right = solve(monkey_2)
 
-    x = 1e12
+    known['humn'] = 1j
+    left = solve(monkey_1)
 
-    known['humn'] = 0; a = solve(monkey_1)
-    known['humn'] = x; y = solve(monkey_1)
-
-    b = (y - a) / x
-    ans = (right - a) / b
-
-    return int(round(ans))
+    return int((right - left.real) / left.imag)
 
 
 if __name__ == '__main__':
